d40cad673df143a53b671aaf91940033a9f55c0b
napen123 <>
2022-06-14 18:22:03 -0600

    Fix incorrect Move optimization by preventing wraps

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -70,8 +70,15 @@ fn squash_and_clean(instructions: &mut Vec<Instruction>, buffer: &mut Vec<Instru
                     let mut accumulator = start_amount;
 
                     while let Some(Instruction::Move(next_amount)) = iterator.peek() {
-                        accumulator = accumulator.wrapping_add(*next_amount);
-                        iterator.next();
+                        match accumulator.checked_add(*next_amount) {
+                            Some(new_accumulator) => {
+                                accumulator = new_accumulator;
+                                iterator.next();
+                            }
+                            None => {
+                                break;
+                            }
+                        }
                     }
 
                     if accumulator != 0 {
